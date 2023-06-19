a7ad9636563ccce90226a0aaf66fd14d250441a6
jungmin <38978923+oxxsusu@users.noreply.github.com>
2023-06-19 18:42:06 +0900

    [Silver II] Title: 섬의 개수, Time: 96 ms, Memory: 34160 KB -BaekjoonHub

--- 백준/Silver/4963. 섬의 개수/섬의 개수.py
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.py
@@ -1,38 +1,31 @@
 from collections import deque
 
+dx, dy = [-1,-1,-1,0,0,1,1,1], [-1,0,1,-1,1,-1,0,1]
+
 def bfs(x, y):
-    diff = [[-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1]]
-    
     q = deque()
     q.append((x, y))
-    maps[x][y] = 0 # 방문처리
-    
+
     while q:
-        now = q.popleft()
-        
+        x, y = q.popleft()
         for i in range(8):
-            nx = now[0] + diff[i][0]
-            ny = now[1] + diff[i][1]
-            if 0 <= nx < h and 0 <= ny < w:
-                if maps[nx][ny] == 1:
-                    maps[nx][ny] = 0
-                    q.append((nx, ny))
-                    
+            nx, ny = x+dx[i], y+dy[i]
+            if 0<=nx<h and 0<=ny<w and graph[nx][ny] == 1:
+                graph[nx][ny] = 0
+                q.append((nx, ny))
+
 while True:
     w, h = map(int, input().split())
-    maps = []
-    ans = 0
-    
-    if w == h == 0:
+    cnt = 0
+    if w==0 and h==0:
         break
-        
-    for _ in range(h):
-        maps.append(list(map(int, input().split())))
-        
-    for i in range(h):
-        for j in range(w):
-            if maps[i][j] == 1:
-                ans += 1
-                bfs(i, j)
-                
-    print(ans)
+
+    graph = [list(map(int, input().split())) for _ in range(h)]
+
+    for x in range(h):
+        for y in range(w):
+            if graph[x][y]==1:
+                cnt += 1
+                bfs(x, y)
+
+    print(cnt)
