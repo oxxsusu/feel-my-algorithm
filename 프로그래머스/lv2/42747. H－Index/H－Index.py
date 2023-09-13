b9b7a9388210613281f7f4f551582f6c62cc18b8
jungmin <38978923+oxxsusu@users.noreply.github.com>
2023-09-13 23:25:27 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42747. H－Index/H－Index.py
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -1,23 +1,11 @@
 def solution(citations):
-    H_index = []
-
-    for h in range(len(citations), -1, -1):
-        upper = [x for x in citations if x >= h]
-        lower = [x for x in citations if x not in upper]
+    answer = 0
+    citations.sort()
+    n = len(citations)
+    
+    for h in range(max(citations)+1):
+        up = len([c for c in citations if c >= h])
+        down = len([c for c in citations[:n-up] if c <= h])
+        if up >= h and len(citations[:n-up]) == down: answer = h
         
-        lower.sort(reverse=True)
-        if len(upper) >= h:
-            if lower and lower[0] <= h:
-                H_index.append(h)
-                break
-            
-            if not lower and h != 0:
-                H_index.append(h)
-                break
-                
-    if len(H_index)==0:
-        answer = 0
-    else:
-        answer = max(H_index)
-                
     return answer
